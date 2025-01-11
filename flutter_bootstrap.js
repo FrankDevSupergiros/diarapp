b4ec58a969efdada577a8db58601c4ca83cc4e49
frank <frankie.5819@gmail.com>
2025-01-10 21:57:34 -0500

    se agregan las vistas de las cuotas

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2724133484"
+    serviceWorkerVersion: "3888687350"
   }
 });
