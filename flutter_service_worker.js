f183ecedbe5e8e2246a4411ef3c642816b69eacf
frank <frankie.5819@gmail.com>
2025-04-23 22:19:26 -0500

    se corrige descuento de las cuotas cuando se paga

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -44,16 +44,16 @@ const RESOURCES = {".git/config": "920a11de313bfb8d93d81f4a3a5b71b6",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "990dcf862c8b28d6c71b18af43121ae3",
+"flutter_bootstrap.js": "1f60e00579dca73406e1754f7018ff55",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "225d2d98937dcad6c893c5e65870c4f7",
 "/": "225d2d98937dcad6c893c5e65870c4f7",
-"main.dart.js": "97da53cd84778b2c3975912464b71032",
+"main.dart.js": "186b2e49e0ee7a848f702c9090ff30ef",
 "manifest.json": "45f15ef2a739d2f08327ad4c3957e688",
-"version.json": "12edae75b2a7e1bcc11584b3a749a3b8"};
+"version.json": "003ffa95607044daf37f28d57b63f3cb"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
